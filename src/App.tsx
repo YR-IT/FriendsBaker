7e01838c749c9940b40e00d810d64dfeb6b71dec
Sukhmeet Singh Oberoi <sukhmeetoberoi@gmail.com>
2025-08-17 15:48:33 +0530

    change

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,22 @@
 
-import BestSellers from "./components/BestSeller";
-import CTASection from "./components/CTA";
-import Footer from "./components/Footer";
-import GiftHamper from "./components/GiftHamper";
-import Header from "./components/Header";
-import Hero from "./components/Hero";
-import Menu from "./components/Menu";
-import TeaTimeDelights from "./components/TeaTimeDelights";
-import Testimonials from "./components/Testimonials";
+// import BestSellers from "./components/BestSeller";
+// import CTASection from "./components/CTA";
+import Dashboard from "./components/Dashboard";
+// import Footer from "./components/Footer";
+// import GiftHamper from "./components/GiftHamper";
+// import Header from "./components/Header";
+// import Hero from "./components/Hero";
+// import Menu from "./components/Menu";
+// import TeaTimeDelights from "./components/TeaTimeDelights";
+// import Testimonials from "./components/Testimonials";
 
 function App() {
   return (
      <div className="min-h-screen flex flex-col overflow-hidden">
-      <Header />
       <main className="flex-grow">
-          <Hero />
-          <Menu />
-          <BestSellers />
-          <GiftHamper />
-          <TeaTimeDelights />
-          <Testimonials />
-          <CTASection />
+      <Dashboard />
       </main>
-      <Footer />
+
     </div>
   );
 }
