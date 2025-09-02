28bc4c0b616430757f845e7fda944dc8f18744b3
Neha Garg <gargneha2409@gmail.com>
2025-09-02 21:14:51 +0530

    vercel error

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,6 @@
 import { motion } from "framer-motion";
 import { useState, useEffect } from "react";
-import { Instagram, Facebook, Phone, MessageCircle, } from "lucide-react";
+import { Instagram, Facebook, Phone, } from "lucide-react";
 import { FaWhatsapp } from "react-icons/fa";
 
 function Hero() {
